facad3f95ea5e1cee7fd5583af5aef8013e0efb7
KPR <64140329+kpproce@users.noreply.github.com>
2024-10-03 11:32:26 +0200

    versie 1.31_21 3 okt responsive en swipe update

--- getCommitMessage.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-
-try {
-    const commitMessage = execSync('git log -1 --pretty=%B').toString().trim();
-    console.log(commitMessage);
-} catch (error) {
-    console.error('Failed to get commit message:', error);
-    process.exit(1);
-}
