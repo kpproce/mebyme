8df50f087d979f52834b26ffa0469a6c89cdd14b
KPR <64140329+kpproce@users.noreply.github.com>
2024-10-17 13:12:00 +0200

    >Versie 1.34.03 MedIcon in slider 17 okt gem zwaarder

--- src/components/myUsers_info.jsx
+++ src/components/myUsers_info.jsx
@@ -123,7 +123,7 @@ const MyUsers = (props) => {
 
   return (
     <>
-      <p>Versie 1.34 MedIcon in slider 17 okt</p>
+      <p>>Versie 1.34.02 MedIcon in slider 17 okt gem zwaarder</p>
       {/* {console.log('142:')} */}
       {/* {console.log(data)} */}
       <h2 className="smallTitle"> usergegevens, opgevraagd door {props.username}</h2> 
